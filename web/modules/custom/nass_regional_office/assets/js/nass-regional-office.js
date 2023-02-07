c1624577173524ff272642463cd8d2337964a1c5
Jason Glisson <jglisson@ip-10-0-0-114.ec2.internal>
2023-02-07 08:47:17 -0500

    Tweaks to styles

--- web/modules/custom/nass_regional_office/assets/js/nass-regional-office.js
+++ web/modules/custom/nass_regional_office/assets/js/nass-regional-office.js
@@ -19,7 +19,6 @@
           state = states[x];
           $('#' + state + ' path').css({ fill: color[0]['color'] });
           $('#' + state).addClass(class_name);
-
         }
       }
 
@@ -34,7 +33,43 @@
         });
       });
 
+      $('.office-list-row').each(function () {
+        var text = $(this).find('span').text();
+        text = text.replace(/\s+/g, '-').toLowerCase();
+        $(this).addClass(text).attr('tabindex', '0');
+        console.log(text);
+      });
+
+      $(document).on('click', '.office-list-row', function (e) {
+        e.preventDefault();    
+        var target = $(this).attr('class').split(' ');
+        var selector = target[2];
+        var scrollTarget = $('#'+selector);
+        $('html, body').stop().animate({
+          'scrollTop': scrollTarget.offset().top
+        }, 700, 'swing', function () {
+          window.location.hash = target;
+        });
+      });
+
       $('.paragraph--type--regional-office:last').addClass('last');
+
+      $('#statesList').on('change', function() {
+        var value = $(this).val();
+        $('#' + value).trigger('click');
+      });
+
+      $('#States g').on('keypress', function(e) {
+        if(e.which === 13) {
+          $(this).trigger('click');
+        }
+      });
+
+      $('#block-views-block-regional-office-list-block-1 .office-list-row').on('keypress', function(e) {
+        if(e.which === 13) {
+          $(this).trigger('click');
+        }
+      });      
 
     }
   }
