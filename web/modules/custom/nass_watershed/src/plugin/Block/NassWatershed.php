c443f9981f3a444960f820ced6518c92126a85f4
Jason Glisson <jglisson@ip-10-0-0-114.ec2.internal>
2023-04-24 11:06:00 -0400

    Adding watershed map into configs

--- web/modules/custom/nass_watershed/src/plugin/Block/NassWatershed.php
+++ web/modules/custom/nass_watershed/src/plugin/Block/NassWatershed.php
@@ -12,7 +12,7 @@ use Drupal\Core\Session\AccountInterface;
  *
  * @Block(
  *   id = "nass_watershed",
- *   admin_label = @Translation("NASS Watershed")
+ *   admin_label = @Translation("NASS Watershed"),
  * )
  */
 class NassWatershed extends BlockBase {
