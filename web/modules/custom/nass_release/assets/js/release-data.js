945f80cbae64bdb1dd1111477efd5da88c6a51c7
Jason Glisson <jglisson@ip-10-0-0-114.ec2.internal>
2023-06-08 21:46:09 -0400

    Adding timer styles

--- web/modules/custom/nass_release/assets/js/release-data.js
+++ web/modules/custom/nass_release/assets/js/release-data.js
@@ -18,6 +18,21 @@
       var todayswrap = $('#todays-release-wrap');
       var upcomingwarp = $('#upcoming-release-data');
 
+      function convertTimestamp(timestamp) {
+        var d = new Date(timestamp * 1000), // Convert the passed timestamp to milliseconds
+            yyyy = d.getFullYear(),
+            mm = ('0' + (d.getMonth() + 1)).slice(-2),  // Months are zero based. Add leading 0.
+            dd = ('0' + d.getDate()).slice(-2),         // Add leading 0.
+            hh = d.getHours(),
+            h = hh,
+            min = ('0' + d.getMinutes()).slice(-2),     // Add leading 0.
+            sec = ('0' + d.getSeconds()).slice(-2),     // Add leading 0.
+            time;
+    
+        time = yyyy + '-' + mm + '-' + dd + ' ' + h + ':' + min + ':' + sec;
+        return time;
+    }
+
       for (var i = 0; i < releaseData.length; i++) {
         todaysData = releaseData[i];
         todayTime = todaysData['time'];
@@ -26,11 +41,11 @@
         filename = filename[0]['value'];
         now = todaysData['now'];
         fulldate = todaysData['full_datetime'];
-        if (now >= fulldate) {
-          todayswrap.append('<div class="release-item-info" data-time="' + todayTime + '"><div class="release-time-title"><div class="release-date-time">' + todayTime + '</div><h4><a href="/data-and-statistics/todays-releases">' + todayTitle + '</a></h4></div><ul class="usa-button-group usa-button-group--segmented release-data"><li class="usa-button-group__item"><button type="button" class="usa-button usa-button--outline"><a href="https://release.nass.usda.gov/reports/'+ filename +'.txt" target="_blank">Text</a></button></li><li class="usa-button-group__item"><button type="button" class="usa-button usa-button--outline"><a href="https://release.nass.usda.gov/reports/'+ filename +'.pdf" target="_blank">PDF</a></button></li><li class="usa-button-group__item"><button type="button" class="usa-button usa-button--outline"><a href="https://release.nass.usda.gov/reports/'+ filename +'.zip" target="_blank">CSV</a></button></li></ul></div>');
-        } else {
-          todayswrap.append('<div class="release-item-info" data-time="' + todayTime + '"><div class="release-time-title"><div class="release-date-time">' + todayTime + '</div><h4><a href="/data-and-statistics/todays-releases">' + todayTitle + '</a></h4></div><ul class="usa-button-group usa-button-group--segmented release-data"><li class="usa-button-group__item"><span>Pending report...</span></li></ul></div>');
-        }
+        // if (now >= fulldate) {
+        //   todayswrap.append('<div class="release-item-info" data-time="' + todayTime + '"><div class="release-time-title"><div class="release-date-time">' + todayTime + '</div><h4><a href="/data-and-statistics/todays-releases">' + todayTitle + '</a></h4></div><ul class="usa-button-group usa-button-group--segmented release-data"><li class="usa-button-group__item"><button type="button" class="usa-button usa-button--outline"><a href="https://release.nass.usda.gov/reports/'+ filename +'.txt" target="_blank">Text</a></button></li><li class="usa-button-group__item"><button type="button" class="usa-button usa-button--outline"><a href="https://release.nass.usda.gov/reports/'+ filename +'.pdf" target="_blank">PDF</a></button></li><li class="usa-button-group__item"><button type="button" class="usa-button usa-button--outline"><a href="https://release.nass.usda.gov/reports/'+ filename +'.zip" target="_blank">CSV</a></button></li></ul></div>');
+        // } else {
+        //   todayswrap.append('<div class="release-item-info" data-time="' + todayTime + '"><div class="release-time-title"><div class="release-date-time">' + todayTime + '</div><h4><a href="/data-and-statistics/todays-releases">' + todayTitle + '</a></h4></div><ul class="usa-button-group usa-button-group--segmented release-data"><li class="usa-button-group__item"><span>Pending report...</span></li></ul></div>');
+        // }
       }
 
       var releaseToday = releaseData[0]['full_datetime'];
@@ -62,11 +77,8 @@
             if (hours < "10") { hours = "0" + hours; }
             if (minutes < "10") { minutes = "0" + minutes; }
             if (seconds < "10") { seconds = "0" + seconds; }
-      
-            // $('#timer-' + i).find('.days').html(days + "<span>Days</span>");
-            // $('#timer-' + i).find('.hours').html(hours + "<span>Hours</span>");
-            // $('#timer-' + i).find('.minutes').html(minutes + "<span>Minutes</span>");
-            // $('#timer-' + i).find('.seconds').html(seconds + "<span>Seconds</span>");		
+
+            now = convertTimestamp(now);
 
             if (now >= fulldate) {
               todayswrap.find('div[data-time="' + todayTime + '"]').remove();
