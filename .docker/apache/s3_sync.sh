34e51ba0a0197f6d130a3d5afc3e9486fa0035e4
Joe Chang <joe.chang@steampunk.com>
2023-08-22 10:36:38 -0400

    WebMod: Cleanup Dockerfile and s3_sync.sh

--- .docker/apache/s3_sync.sh
+++ .docker/apache/s3_sync.sh
@@ -3,5 +3,7 @@
 # Sync files from S3 bucket to the local directory
 aws s3 sync s3://$S3_BUCKET_NAME $LOCAL_DIR
 
-# Start Apache server
-httpd-foreground
+echo "S3 synchronization complete"
+
+# Restart Apache after synchronization (if needed)
+apachectl restart
