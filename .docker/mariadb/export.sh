d991ea1a559f05973ab4002698a804f290b2a46d
Max Saperstone <max.saperstone@steampunk.com>
2023-08-24 18:53:50 -0400

    Cleaning up files

--- .docker/mariadb/export.sh
+++ .docker/mariadb/export.sh
@@ -4,7 +4,6 @@ set -e
 
 FILE="/home/export-${MYSQL_DATABASE}-$(date '+%Y-%m-%d').sql"
 
-mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} > "${FILE}"
-#echo "mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE}" >> "${FILE}"
+mysqldump -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" "${MYSQL_DATABASE}" > "${FILE}"
 
-aws s3 cp "$FILE" s3://"${AWS_S3_BUCKET}"
+aws s3 cp "${FILE}" s3://"${AWS_S3_BUCKET}"
