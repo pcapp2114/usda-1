aaf57ad55722576439e448d751fbd5b22b8c8688
Max Saperstone <max.saperstone@steampunk.com>
2023-09-05 11:45:53 -0400

    Setting up structure for QA/Staging containers

---
+++ .docker/apache-qa/s3_sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /home/setEnv.sh
+
+FILE="${DOCROOT}/sites/default/files"
+
+# Sync files from local directory to S3 bucket
+aws s3 sync "$FILE" "s3://${AWS_S3_BUCKET}" --no-progress >> /home/cron.log 2>&1
+
+echo "S3 Sync completed at $(date)" >> /home/cron.log 2>&1
