7c0b0cfb2078540776042f75e4a024711e31c9a2
Jason Glisson <jason.glsson@gmail.com>
2023-10-20 09:57:02 -0400

    Changes to row count for upcoming releases

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'drupal/recommended-project',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '9a9ecc893ab697e9e6e1f78ac709b2a798359c84',
+        'reference' => 'f3a599a2b160a74b2d71feea90cdc95ac7d99683',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -927,7 +927,7 @@
         'drupal/recommended-project' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '9a9ecc893ab697e9e6e1f78ac709b2a798359c84',
+            'reference' => 'f3a599a2b160a74b2d71feea90cdc95ac7d99683',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
