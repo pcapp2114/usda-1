938b8aa07fb0e7938d2cf9b121fd6a4df6d534a0
Jason Glisson <jason.glsson@gmail.com>
2023-10-26 15:46:28 -0400

    Adding modules and view for cropscape metadata

---
+++ web/modules/contrib/content_export_csv/src/Form/ContentExportForm.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Drupal\content_export_csv\Form;
+
+use Drupal\content_export_csv\ContentExport;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StreamWrapper\PrivateStream;
+use Drupal\Core\StreamWrapper\PublicStream;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Content export form.
+ */
+class ContentExportForm extends FormBase {
+
+  /**
+   * The content export service.
+   *
+   * @var \Drupal\content_export_csv\ContentExport
+   */
+  protected $contentExport;
+
+  /**
+   * The constructor.
+   *
+   * @param \Drupal\content_export_csv\ContentExport $content_export
+   *   The content export service.
+   */
+  public function __construct(ContentExport $content_export) {
+    $this->contentExport = $content_export;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('content_export_csv.export')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'content_export_csv_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['content_export'] = [
+      '#title' => $this->t('Export options'),
+      '#type' => 'fieldset',
+    ];
+
+    $form['content_export']['content_type'] = [
+      '#title' => $this->t('Content type'),
+      '#description' => $this->t('Filter nodes by specific content type.'),
+      '#type' => 'select',
+      '#empty_option' => $this->t('- Select -'),
+      '#options' => $this->contentExport->getContentTypes(),
+      '#required' => TRUE,
+      '#ajax' => [
+        'callback' => '::ajaxUpdateContentFields',
+        'wrapper' => 'content-fields-wrapper',
+      ],
+    ];
+
+    $form['content_export']['fields_wrapper'] = [
+      '#type' => 'container',
+      '#id' => 'content-fields-wrapper',
+    ];
+
+    $nodeType = $form_state->getValue('content_type');
+    if (!empty($nodeType)) {
+      $validFields = $this->contentExport->getValidFieldList($nodeType);
+      $fieldOptions = [];
+      foreach ($validFields as $field) {
+        $fieldOptions[$field] = $field;
+      }
+      $form['content_export']['fields_wrapper']['fields'] = [
+        '#title' => $this->t('Content fields'),
+        '#description' => $this->t('Select which field(s) you want to export.'),
+        '#type' => 'select',
+        '#multiple' => TRUE,
+        '#size' => 20,
+        '#options' => $fieldOptions,
+      ];
+    }
+
+    $form['content_export']['status'] = [
+      '#title' => $this->t('Content status'),
+      '#description' => $this->t('Filter nodes that are published or unpublished.'),
+      '#type' => 'select',
+      '#empty_option' => $this->t('- Select -'),
+      '#options' => [
+        0 => $this->t('Unpublished'),
+        1 => $this->t('Published'),
+      ],
+      '#default_value' => 1,
+    ];
+
+    $form['content_export']['include_node_urls'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Include Node Urls at the end of the exported CSV row.'),
+      '#default_value' => 0,
+    ];
+
+    $form['content_export']['export'] = [
+      '#value' => $this->t('Export content'),
+      '#type' => 'submit',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Ajax callback that updates available content fields.
+   *
+   * @param array $form
+   *   The form render array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The current request.
+   */
+  public function ajaxUpdateContentFields(array &$form, FormStateInterface $form_state, Request $request) {
+    return $form['content_export']['fields_wrapper'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $nodeType = $form_state->getValue('content_type');
+    $status = $form_state->getValue('status');
+    $fields = $form_state->getValue('fields');
+    $include_node_urls = $form_state->getValue('include_node_urls');
+
+    $private_path = PrivateStream::basepath();
+    $public_path = PublicStream::basepath();
+    $file_base = ($private_path) ? $private_path : $public_path;
+    $filename = 'content_export' . time() . '.csv';
+    $filepath = $file_base . '/' . $filename;
+    $csvFile = fopen($filepath, "w");
+
+    if (!empty($fields)) {
+      $fieldNames = implode(',', $fields);
+    }
+    else {
+      $fieldNames = implode(',', $this->contentExport->getValidFieldList($nodeType));
+    }
+
+    // Include Node Urls at the end of the exported CSV row.
+    if ($include_node_urls) {
+      $fieldNames .= ',url';
+    }
+
+    $csvData = $this->contentExport->getNodeCsvData($nodeType, $status, $fields, $include_node_urls);
+    fwrite($csvFile, $fieldNames . "\n");
+    foreach ($csvData as $csvDataRow) {
+      fwrite($csvFile, $csvDataRow . "\n");
+    }
+    fclose($csvFile);
+
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="' . basename($filepath) . '";');
+    header('Content-Length: ' . filesize($filepath));
+    readfile($filepath);
+    unlink($filepath);
+    exit;
+  }
+
+}
---
+++ web/modules/contrib/permissions_by_term/modules/permissions_by_entity/src/Service/AccessChecker.php
@@ -0,0 +1,286 @@
+<?php
+
+namespace Drupal\permissions_by_entity\Service;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\permissions_by_entity\Event\EntityFieldValueAccessDeniedEvent;
+use Drupal\permissions_by_entity\Event\PermissionsByEntityEvents;
+use Drupal\permissions_by_term\Service\AccessCheck;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Contains functions to check access permissions on a given entity.
+ *
+ * @package Drupal\permissions_by_entity\Service
+ */
+class AccessChecker extends AccessCheck implements AccessCheckerInterface {
+
+  /**
+   * The event dispatcher.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  private $eventDispatcher;
+
+  /**
+   * The cache for checked entities.
+   *
+   * @var \Drupal\permissions_by_entity\Service\CheckedEntityCache
+   */
+  private $checkedEntityCache;
+
+  /**
+   * The config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  private ConfigFactoryInterface $configFactory;
+
+  /**
+   * The entity field value access denied event.
+   *
+   * @var \Drupal\permissions_by_entity\Event\EntityFieldValueAccessDeniedEvent
+   */
+  private $event;
+
+  /**
+   * Constructs the AccessChecker.
+   */
+  public function __construct(
+    EventDispatcherInterface $event_dispatcher,
+    CheckedEntityCache $checked_entity_cache,
+    EntityFieldManagerInterface $entityFieldManager,
+    Connection $database,
+    ConfigFactoryInterface $config_factory
+  ) {
+    parent::__construct($database, $event_dispatcher, $entityFieldManager);
+    $this->eventDispatcher = $event_dispatcher;
+    $this->checkedEntityCache = $checked_entity_cache;
+    $this->configFactory = $config_factory;
+
+    $this->event = new EntityFieldValueAccessDeniedEvent();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isAccessAllowed(FieldableEntityInterface $entity, $uid = FALSE): bool {
+    $permissions_by_term_settings = $this->configFactory->get('permissions_by_term.settings');
+    $config_permission_mode = $permissions_by_term_settings->get('permission_mode');
+    $config_require_all_terms_granted = $permissions_by_term_settings->get('require_all_terms_granted');
+
+    $access_allowed = FALSE;
+    if (!$config_permission_mode && !$config_require_all_terms_granted) {
+      $access_allowed = TRUE;
+    }
+
+    // Iterate over the fields the entity contains.
+    foreach ($entity->getFields() as $field) {
+
+      // We only need to check for entity reference fields
+      // which references to a taxonomy term.
+      if (
+        $field->getFieldDefinition()->getType() == 'entity_reference' &&
+        $field->getFieldDefinition()->getSetting('target_type') == 'taxonomy_term'
+      ) {
+
+        // Iterate over each referenced taxonomy term.
+        /** @var \Drupal\Core\Field\FieldItemInterface $item */
+        foreach ($field->getValue() as $item) {
+          $access_allowed = !empty($item['target_id']) &&
+            $this->isAccessAllowedByDatabase($item['target_id'], $uid, $entity->language()->getId());
+
+          if (!$access_allowed && $config_require_all_terms_granted) {
+            return $access_allowed;
+          }
+          if ($access_allowed && !$config_require_all_terms_granted) {
+            return $access_allowed;
+          }
+        }
+      }
+
+      // Check if the field contains another fieldable entity,
+      // that we need to check.
+      if (isset($field->entity) && $field->entity instanceof FieldableEntityInterface) {
+
+        // We need to iterate over the entities.
+        $num_values = $field->count();
+        if ($num_values > 0) {
+
+          // Iterate over the field values.
+          for ($i = 0; $i < $num_values; $i++) {
+
+            // Get the value of the current field index.
+            $field_value = $field->get($i);
+
+            // If the value is null or empty we continue with the next index of
+            // the loop.
+            if (!$field_value) {
+              continue;
+            }
+
+            // If the field entity is null we also continue with the next index
+            // of the loop.
+            if (!isset($field_value->entity)) {
+              continue;
+            }
+
+            // Get the field entity.
+            $field_entity = $field_value->entity;
+
+            // It is possible, that the referenced field entity creates a
+            // circular dependency to the current entity. This will cause
+            // memory limit exhausted errors because there is no way out for
+            // the script. To avoid this, we need to be able to imagine if we
+            // already checked this field entity before. If so, we ignore this
+            // field entity, if not we can securely do a recursive call.
+            //
+            // Using own method to avoid "max nesting level error" trying to
+            // check if the field entity is stored in the entitiesChecked array.
+            if ($this->checkedEntityCache->isChecked($field_entity)) {
+              continue;
+            }
+            else {
+              // Add the current entity to the list of checked entities.
+              $this->checkedEntityCache->add($field_entity);
+            }
+
+            // Do a recursive call to check if the user is allowed to access
+            // this entity.
+            if (!$this->isAccessAllowed($field_entity, $uid)) {
+
+              // Dispatch an event to allow subscribers
+              // to do something in this case.
+              $this->event->setIndex($i);
+              $this->event->setField($field);
+              $this->event->setEntity($field_entity);
+              $this->event->setUid($uid);
+
+              $this->eventDispatcher
+                ->dispatch(
+                  $this->event,
+                  PermissionsByEntityEvents::ENTITY_FIELD_VALUE_ACCESS_DENIED_EVENT
+                );
+              $i = $this->event->getIndex();
+            }
+          }
+        }
+      }
+    }
+
+    return $access_allowed;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isAccessControlled(FieldableEntityInterface $entity, bool $clearCache = TRUE): bool {
+    $permissions_by_term_settings = $this->configFactory->get('permissions_by_term.settings');
+
+    if ($clearCache) {
+      $this->checkedEntityCache->clear();
+    }
+
+    if ($entity->getEntityTypeId() == 'node') {
+      // Make sure to leave nodes to the permissions_by_term module.
+      return FALSE;
+    }
+
+    // Iterate over the fields the entity contains.
+    foreach ($entity->getFields() as $field) {
+      // We only need to check for entity reference fields
+      // which references to a taxonomy term.
+      $field_definition = $field->getFieldDefinition();
+      if (
+        $field_definition->getType() === 'entity_reference' &&
+        $field_definition->getSetting('target_type') === 'taxonomy_term'
+      ) {
+        $target_bundles = $permissions_by_term_settings->get('target_bundles');
+        $config_permission_mode = $permissions_by_term_settings->get('permission_mode');
+        $field_target_bundles = $field_definition->getSetting('handler_settings')['target_bundles'];
+        if (
+          count($target_bundles) > 0 &&
+          count(array_intersect($field_target_bundles, $target_bundles)) > 0
+        ) {
+          // Permission mode is turned on, so any taxonomy field
+          // that is valid to be targeted is automatically controlled.
+          if ($config_permission_mode) {
+            return TRUE;
+          }
+
+          // Iterate over each referenced taxonomy term.
+          /** @var \Drupal\Core\Field\FieldItemInterface $item */
+          foreach ($field->getValue() as $item) {
+            if (
+              !empty($item['target_id']) &&
+              $this->isAnyPermissionSetForTerm($item['target_id'], $entity->language()->getId())
+            ) {
+              return TRUE;
+            }
+          }
+        }
+      }
+
+      // Check if the field contains another fieldable entity,
+      // that we need to check.
+      if (isset($field->entity) && $field->entity instanceof FieldableEntityInterface) {
+
+        // We need to iterate over the entities.
+        $num_values = $field->count();
+        if ($num_values > 0) {
+
+          // Iterate over the field values.
+          for ($i = 0; $i < $num_values; $i++) {
+
+            // Get the value of the current field index.
+            $field_value = $field->get($i);
+
+            // If the value is null or empty we continue with the next index of
+            // the loop.
+            if (!$field_value) {
+              continue;
+            }
+
+            // If the field entity is null we also continue with the next index
+            // of the loop.
+            if (!isset($field_value->entity)) {
+              continue;
+            }
+
+            // Get the field entity.
+            $field_entity = $field_value->entity;
+
+            // It is possible, that the referenced field entity creates a
+            // circular dependency to the current entity. This will cause
+            // memory limit exhausted errors because there is no way out for
+            // the script. To avoid this, we need to be able to imagine if we
+            // already checked this field entity before. If so, we ignore this
+            // field entity, if not we can securely do a recursive call.
+            //
+            // Using own method to avoid "max nesting level error" trying to
+            // check if the field entity is stored in the entitiesChecked array.
+            if ($this->checkedEntityCache->isChecked($field_entity)) {
+              continue;
+            }
+            else {
+              // Add the current entity to the list of checked entities.
+              $this->checkedEntityCache->add($field_entity);
+            }
+
+            // Do a recursive call to check if the user is allowed to access
+            // this entity.
+            if ($this->isAccessControlled($field_entity, FALSE)) {
+              return TRUE;
+            }
+          }
+        }
+      }
+    }
+
+    return FALSE;
+  }
+
+}
