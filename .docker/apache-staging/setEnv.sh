91c919a339fff9696538a31665ec7dd6dcd1b10b
Zach Alsgaard <zacharia.alsgaard@usda.gov>
2023-11-06 14:52:28 -0500

    Another way of copying the s3_sync sh file to /home

--- .docker/apache-staging/setEnv.sh
+++ .docker/apache-staging/setEnv.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+set -e
+
+echo "Starting entrypoint.sh..."
+
+echo "Starting SSH..."
+/usr/sbin/sshd
+echo "Finished starting SSH..."
 
 printenv | grep DOCROOT > /home/.env
 printenv | grep AWS >> /home/.env
