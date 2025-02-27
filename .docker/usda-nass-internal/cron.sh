12689f5ae75a403015001671b6d9b7784c460be0
Celso Castedo Hallen <Celso.CastedoHallen@usda.gov>
2025-02-26 22:56:32 -0500

    Testing pipeline

--- .docker/usda-nass-internal/cron.sh
+++ .docker/usda-nass-internal/cron.sh
@@ -2,13 +2,8 @@
 # Set Crontabs
 
 {
-  echo "* * * * * wget -q -O /dev/null http://internaldrupaldev.nass.usda.gov/cron/A2ygWfkVpqGLIpf3hob1BXABU1YDJgjuZyJW4vJ4iEApUAZQZEJX9KfgfN4uRNd6FxXhuEc8lg"
+  echo '* * * * * wget -q -O /dev/null "http://internaldrupaldev.nass.usda.gov/cron/A2ygWfkVpqGLIpf3hob1BXABU1YDJgjuZyJW4vJ4iEApUAZQZEJX9KfgfN4uRNd6FxXhuEc8lg"'
 } >>setcron
 
 crontab setcron
 rm setcron
-
-#cd /var/www/html || exit
-#drush cr
-#drush updb -y
-#drush cim -y
